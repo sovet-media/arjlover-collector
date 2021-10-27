cc37e6a894084169c9e56d1c7ea78aadcdc0bad3
Andrew <rembozebest@gmail.com>
2021-10-27 22:23:04 +0300

    prepare to next version

--- props.py
+++
@@ -1,6 +0,0 @@
-DB_VERSION = 1
-TIMEOUT_S = 4
-API_KEY_kinopoiskapiunofficial_tech = ''
-SAVE_RESULT_TO = 'collector.json'
-ALLOWED_COUNTRIES = ['СССР']
-ALLOWED_GENRES = ['мультфильм', 'короткометражка']
